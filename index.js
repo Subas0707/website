e64bc5d832f99ab7c13a309e2d7a2747a2c68d37
callback cat <76839908+chamanbravo@users.noreply.github.com>
2023-12-10 19:00:09 +0545

    feat: first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+const updatePhysicsEngine = (mode) => {
+  const background = mode === "dark" ? "#000" : "#fff";
+  render.options.background = background;
+  leftWall.render.fillStyle =
+    rightWall.render.fillStyle =
+    topWall.render.fillStyle =
+    bottomWall.render.fillStyle =
+    ground.render.fillStyle =
+      background;
+};
+
+const matcher = window.matchMedia("(prefers-color-scheme: dark)");
+if (
+  localStorage.getItem("data-color-mode") === "dark" ||
+  (matcher.matches && !localStorage.getItem("data-color-mode"))
+) {
+  document.documentElement.setAttribute("data-color-mode", "dark");
+  localStorage.setItem("data-color-mode", "dark");
+}
+
+document.getElementById("enable_dark_mode").addEventListener("click", () => {
+  document.documentElement.setAttribute("data-color-mode", "dark");
+  localStorage.setItem("data-color-mode", "dark");
+  updatePhysicsEngine("dark");
+});
+
+document.getElementById("enable_light_mode").addEventListener("click", () => {
+  document.documentElement.setAttribute("data-color-mode", "light");
+  localStorage.setItem("data-color-mode", "light");
+  updatePhysicsEngine("light");
+});
+
+const isDarkMode = () => localStorage.getItem("data-color-mode") === "dark";
+
+// Initialize Matter.js
+const Engine = Matter.Engine,
+  Render = Matter.Render,
+  World = Matter.World,
+  Bodies = Matter.Bodies,
+  Mouse = Matter.Mouse,
+  MouseConstraint = Matter.MouseConstraint;
+
+// Create an engine
+const engine = Engine.create();
+
+// Create a renderer
+const render = Render.create({
+  element: document.querySelector(".canvas-physics"),
+  engine: engine,
+  options: {
+    width: 400,
+    height: 300,
+    wireframes: false,
+    background: isDarkMode() ? "#000" : "#fff",
+  },
+});
+
+// tech stack icons
+const techStack = [];
+
+for (let i = 0; i <= 6; i++) {
+  techStack.push(
+    Bodies.rectangle(200, 200, 45, 45, {
+      render: {
+        sprite: {
+          texture: `./assets/svg/techStack${i}.svg`,
+        },
+      },
+    })
+  );
+}
+
+// Create walls
+const wallOptions = {
+  isStatic: true,
+  render: {
+    fillStyle: isDarkMode() ? "#000" : "#fff",
+  },
+};
+const leftWall = Bodies.rectangle(0, 400 / 2, 20, 400, wallOptions);
+const rightWall = Bodies.rectangle(400, 400 / 2, 20, 400, wallOptions);
+const topWall = Bodies.rectangle(400 / 2, 0, 400, 20, wallOptions);
+const bottomWall = Bodies.rectangle(400 / 2, 400, 400, 20, wallOptions);
+const ground = Bodies.rectangle(400 / 2, 300, 400, 20, wallOptions);
+
+World.add(engine.world, [
+  ...techStack,
+  leftWall,
+  rightWall,
+  topWall,
+  bottomWall,
+  ground,
+]);
+
+// Add gravity
+engine.world.gravity.y = 0.5;
+
+// Make shapes draggable
+const mouse = Mouse.create(render.canvas);
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: mouse,
+  constraint: {
+    stiffness: 0.2,
+    render: {
+      visible: false,
+    },
+  },
+});
+
+World.add(engine.world, mouseConstraint);
+
+// Run the engine
+Matter.Runner.run(engine);
+
+// Run the renderer
+Render.run(render);
